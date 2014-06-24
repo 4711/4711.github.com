4d15d491d2429b32c9d6f252799e2d5fc4ac4225
cfrei <christoph.frei@fela.ch>
2014-06-24 14:46:59 +0200

    Automation with Gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,46 @@
+/* Modules */
+
+var gulp     = require('gulp');
+//var stylus   = require('gulp-stylus');
+var prefixer = require('gulp-autoprefixer');
+var pixrem   = require('gulp-pixrem');
+var minify   = require('gulp-minify-css');
+var exec     = require('gulp-exec');
+
+/* Tasks */
+
+gulp.task('css', function() {
+  return gulp
+    .src('_assets/css/*.css')
+    //.pipe(prefixer('last 2 versions'))
+    //.pipe(pixrem())
+    .pipe(minify())
+    .pipe(gulp.dest('css/'));
+});
+
+gulp.task('img', function() {
+  return gulp
+    .src('_assets/img/*')
+    .pipe(gulp.dest('img/'));
+});
+
+gulp.task('serve', function() {
+  var jekyll = [
+    'jekyll serve',
+    '--watch',
+    '--config',
+    '_config.yml,_local.yml'
+  ].join(' ');
+  gulp.src('./').pipe(exec(jekyll));
+});
+
+gulp.task('watch', function() {
+  gulp.watch('_assets/css/*.css', ['css']);
+});
+
+gulp.task('default', [
+  'css',
+  'img',
+  'serve',
+  'watch'
+]);
